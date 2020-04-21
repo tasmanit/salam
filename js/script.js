5abc1031297d9efecc81f4eabf86773a8a069b7d
tasmanit <63876838+tasmanit@users.noreply.github.com>
2020-04-21 08:13:37 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
-$('#top__slider').owlCarousel({
+$(document).ready(function(){
+    $('#top__slider').owlCarousel({
     loop:true,
     nav:true,
     dots:true,
     items: 1,
-    animateOut: 'fadeOut',
-    animateIn: 'flipInX',
     autoplay:true,
     autoplayTimeout:3000,
     autoplayHoverPause:false
-})
+    })
 
-$('#plan__slider').owlCarousel({
-    nav:true,
-    dots:true,
-    items: 11
-})
+    $('#plan__slider').owlCarousel({
+        nav:true,
+        dots:true,
+        items: 11
+    })
 
- window.addEventListener("scroll", function(){
-            var header = document.querySelector(".mainmenu");
-            header.classList.toggle("sticky", window.scrollY > 0);
-})
+    $('#tiles').owlCarousel({
+        items: 3,
+        autoplay:false
+    })
+
+     window.addEventListener("scroll", function(){
+                var header = document.querySelector(".mainmenu");
+                header.classList.toggle("sticky", window.scrollY > 0);
+    })
+
+     new WOW().init();
+});
+
